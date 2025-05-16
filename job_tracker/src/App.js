df4d6d8fb0d5f25284987fa73779da7c982199b1
Pratham9325 <125430290+Pratham9325@users.noreply.github.com>
2025-05-16 21:32:00 +0530

    add

---
+++ job_tracker/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './component/Header';
+import Dashboard from './component/Dashboard';
+import Footer from './component/Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Dashboard />
+   
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
